414de5d59d0e3e48d1b5f6612195edebdf303fd0
Stoyan <stoyandryanov@gmail.com>
2022-08-19 13:52:27 +0300

    Refactoring

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Rock-Paper-Scissors
 A simple rock paper scissors game against a bot using JavaScript
+DOM Manipulations
+No CSS
--- script.js
+++ script.js
@@ -6,43 +6,52 @@ let getComputerChoice = () => {
     return choices[Math.floor(Math.random() * 3)]
 };
 
-let playRound = () => {
-    let computer = getComputerChoice().toLowerCase();
-    let player = prompt('Enter rock paper or scissors.').toLowerCase();
+let playRound = (playerChoice) => {
+    let computer = getComputerChoice();
+    let player = playerChoice;
 
     if (player === computer) {
-        console.log(`Tie! You both used ${computerSelection}!`)
-        playerWins++;
+        return `Tie! You both used ${computer}!`;
+    } else if (player === 'Rock' && computer === 'Paper') {
         computerWins++;
-    } else if (player === 'rock' && computer === 'paper') {
-        console.log('You lose! Paper beats Rock!');
-        computerWins++;
-    } else if (player === 'rock' && computer === 'scissors') {
-        console.log('You win! Rock beats Scissors!');
+        return 'You lose! Paper beats Rock!';
+    } else if (player === 'Rock' && computer === 'Scissors') {
         playerWins++;
-    } else if (player === 'paper' && computer === 'rock') {
-        console.log('You win! Paper beats Rock!');
+        return 'You win! Rock beats Scissors!';
+    } else if (player === 'Paper' && computer === 'Rock') {
         playerWins++;
-    } else if (player === 'paper' && computer === 'scissors') {
-        console.log('You lose! Scissors beat Paper!');
+        return 'You win! Paper beats Rock!';
+    } else if (player === 'Paper' && computer === 'Scissors') {
         computerWins++;
-    } else if (player === 'scissors' && computer === 'rock') {
-        console.log('You lose! Rock beats Scissors!');
+        return 'You lose! Scissors beat Paper!';
+    } else if (player === 'Scissors' && computer === 'Rock') {
         computerWins++;
-    } else if (player === 'scissors' && computer === 'paper') {
-        console.log('You win! Scissors beat Paper!');
+        return 'You lose! Rock beats Scissors!';
+    } else if (player === 'Scissors' && computer === 'Paper') {
         playerWins++;
+        return 'You win! Scissors beat Paper!';
     }
 }
 
-let game = () => {
-    for (let i = 0; i < 5; i++) {
-        playRound();
-    }
+const buttons = document.querySelectorAll('button');
 
-    if (computerWins > playerWins) {
-        console.log('You lost the game!')
-    } else {
-        console.log('You won the game!')
-    }
-}
+buttons.forEach(option => {
+    option.addEventListener('click', (e) => {
+        const scoreMessage = document.querySelector('.score-message');
+        const score = document.querySelector('.score');
+        const winner = document.querySelector('.winner');
+        
+        winner.textContent = '';
+        scoreMessage.textContent = playRound(option.classList.value);
+        score.textContent = `${playerWins} - ${computerWins}`;
+        if (playerWins === 5 && computerWins < 5) {
+            winner.textContent = 'You Win!';
+            playerWins = 0;
+            computerWins = 0;
+        } else if (computerWins === 5 && playerWins < 5) {
+            winner.textContent = 'You Lose!';
+            playerWins = 0;
+            computerWins = 0;
+        }
+    })
+})
